f5ee3b5648f355790af3c1c3973a90c77d978717
Alvin <lam_hoi_fai@hotmail.com>
2012-12-02 00:16:22 -0800

    Grab data to database

--- Config/application.php
+++ Config/application.php
@@ -1,7 +1,7 @@
 <?php
 // global app config
-Configure::write('App.name', 'Life');
-Configure::write('App.description', 'Life game');
+Configure::write('App.name', 'Pokemon Life');
+Configure::write('App.description', 'Pokemon Life game Beta');
 Configure::write('App.authors', array('Alvin Lam'));
 Configure::write('App.url', 'http://life.localhost');
 
@@ -9,3 +9,7 @@ Configure::write('App.url', 'http://life.localhost');
 Configure::write('Role.master', 1);
 Configure::write('Role.admin', 2);
 Configure::write('Role.agent', 3);
+
+
+Configure::write('Pokedex.source.url', 'http://pokemondb.net/pokedex/all');
+Configure::write('PokemonSkill.source.url', 'http://pokemondb.net/move/generation/1');
